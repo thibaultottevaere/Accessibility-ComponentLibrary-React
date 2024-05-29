8e0f4e4daeea7c23c2f9421a748ec05a5aeb6e6d
Thibault Ottevaere <thibault.ottevaere@student.odisee.be>
2024-05-29 17:51:34 +0200

    update title

--- components-library/public/heading/title.js
+++ components-library/public/heading/title.js
@@ -4,27 +4,27 @@ const Title = ({element, title, style}) => {
     return(
         <>
         {element === "h1" ? 
-            (<h1 aria-roledescription="heading" className={style}>{title}</h1>)
+            (<h1 aria-roledescription="heading" aria-label="title" className={style}>{title}</h1>)
             : <></>
         }
         {element === "h2" ?
-            (<h2 aria-roledescription="heading" className={style}>{title}</h2>)
+            (<h2 aria-roledescription="heading" aria-label="title" className={style}>{title}</h2>)
             : <></>
         }
         {element === "h3" ?
-            (<h3 aria-roledescription="heading" className={style}>{title}</h3>)
+            (<h3 aria-roledescription="heading" aria-label="title" className={style}>{title}</h3>)
             : <></>
         }
         {element === "h4" ?
-            (<h4 aria-roledescription="heading" className={style}>{title}</h4>)
+            (<h4 aria-roledescription="heading" aria-label="title" className={style}>{title}</h4>)
             : <></>
         }
         {element === "h5" ?
-            (<h5 aria-roledescription="heading" className={style}>{title}</h5>)
+            (<h5 aria-roledescription="heading" aria-label="title" className={style}>{title}</h5>)
             : <></>
         }
         {element === "h6" ?
-            (<h6 aria-roledescription="heading" className={style}>{title}</h6>)
+            (<h6 aria-roledescription="heading" aria-label="title" className={style}>{title}</h6>)
             : <></>
         }
         </> 
